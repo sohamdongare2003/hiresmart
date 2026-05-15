0c5a9175f50796743ad761c8d3bcd9b7964d88f2
sohamdongare2003 <sohamdongare70@gmail.com>
2026-05-15 10:18:50 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from typing import List
+import os
 
 
 class Settings(BaseSettings):
@@ -13,14 +14,14 @@ class Settings(BaseSettings):
     DB_HOST: str = "localhost"
     DB_PORT: int = 3306
     DB_USER: str = "root"
-    DB_PASSWORD: str = "Mysql12"
+    DB_PASSWORD: str = ""
     DB_NAME: str = "hiresmart"
 
     @property
     def DATABASE_URL(self) -> str:
         return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    GEMINI_API_KEY: str = "AIzaSyAcsYSPejYymq7H45K5cAF2XHO7OEQxG4I"
+    GEMINI_API_KEY: str = ""
     STORAGE_TYPE: str = "local"
     LOCAL_UPLOAD_DIR: str = "uploads"
 
@@ -29,7 +30,11 @@ class Settings(BaseSettings):
     AWS_REGION: str = "us-east-1"
     S3_BUCKET_NAME: str = "hiresmart-resumes"
 
-    ALLOWED_ORIGINS: List[str] = ["https://hiresmart-seven.vercel.app"]
+    ALLOWED_ORIGINS: str = "http://localhost:3000"
+
+    @property
+    def allowed_origins_list(self) -> List[str]:
+        return [origin.strip() for origin in self.ALLOWED_ORIGINS.split(",")]
 
     class Config:
         env_file = ".env"
