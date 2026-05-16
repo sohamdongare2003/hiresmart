e11999a10729fc40b971270f334833be3dd4f5eb
sohamdongare2003 <sohamdongare70@gmail.com>
2026-05-16 07:29:29 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,14 +14,14 @@ class Settings(BaseSettings):
     DB_HOST: str = "localhost"
     DB_PORT: int = 3306
     DB_USER: str = "root"
-    DB_PASSWORD: str = ""
+    DB_PASSWORD: str = "Mysql12"
     DB_NAME: str = "hiresmart"
 
     @property
     def DATABASE_URL(self) -> str:
         return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    GEMINI_API_KEY: str = ""
+    GEMINI_API_KEY: str = "AIzaSyAcsYSPejYymq7H45K5cAF2XHO7OEQxG4I"
 
     STORAGE_TYPE: str = "local"
     LOCAL_UPLOAD_DIR: str = "uploads"
