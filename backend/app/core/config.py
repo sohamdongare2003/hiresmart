cdb8bebe22e12bd00711609f6100942f892cb078
sohamdongare2003 <sohamdongare70@gmail.com>
2026-05-17 09:13:55 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,18 +5,22 @@ from typing import List
 class Settings(BaseSettings):
     APP_NAME: str = "HireSmart"
 
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    DATABASE_URL: str
+
     ALLOWED_ORIGINS: str = ""
 
     @property
     def allowed_origins_list(self) -> List[str]:
-        return [
-            origin.strip()
-            for origin in self.ALLOWED_ORIGINS.split(",")
-        ]
+        return [origin.strip() for origin in self.ALLOWED_ORIGINS.split(",")]
 
     class Config:
         env_file = ".env"
-        extra = "ignore"
 
 
 settings = Settings()
